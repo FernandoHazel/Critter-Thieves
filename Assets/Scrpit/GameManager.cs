95d4a196809c0c916eb7f7a3555cfb8f0190e8c5
Fernando Hazel Ascencio Baumgarten <fernandohazel1@gmail.com>
2021-04-03 16:50:01 +0000

    Añadido diseño de menú y pausa provisionales

--- Assets/Scrpit/GameManager.cs
+++ Assets/Scrpit/GameManager.cs
@@ -7,6 +7,10 @@ public class GameManager : MonoBehaviour
     //We create a pause, we make it static so that it is the same in all scripts
     public static bool pause = false;
 
+    //this variables holds the different canvas objects of the pause and the running game
+    public GameObject inGame;
+    public GameObject inPause;
+
     // Start is called before the first frame update
     //void Start(){}
 
@@ -19,6 +23,15 @@ public class GameManager : MonoBehaviour
         {
             pause = !pause;
             Debug.Log("pause is " + pause);
+        }
+
+        if (pause){
+            inGame.SetActive(false);
+            inPause.SetActive(true);
+        }
+        if (!pause){
+            inGame.SetActive(true);
+            inPause.SetActive(false);
         }
     }
     
