3910a3adb2810aaa2abae3b9dd7da5d47cf80164
Sergio Ambriz Padilla <sergio.ampa@hotmail.com>
2021-04-06 22:03:26 +0000

    No Comments

---
+++ Assets/Scrpit/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    public void ChangeScene()
+    {
+        Debug.Log("Pico el boton");
+        SceneManager.LoadScene("CritterThievesGame");
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Pico el boton");
+        Application.Quit();
+    }
+
+    void Update()
+    {
+        
+    }
+}
