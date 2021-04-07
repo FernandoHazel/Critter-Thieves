04ab3427fcd37592fe23c5433695efc800a61c8a
Emerson Rios Diaz <rios.emerson29@hotmail.com>
2021-04-07 23:36:39 +0000

    No Comments

---
+++ Assets/Scrpit/Items.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Items : MonoBehaviour
+{
+
+    float rotar = 30;
+    float speed = .03f;
+    float delta = .2f;
+
+    Vector3 posInicial;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        posInicial = transform.position;
+
+    }
+
+    void Rotacion()
+    {
+
+        //transform.rotation(0, _rotationSpeed* Time.deltaTime, 0);
+        transform.Rotate(0, 0, rotar * Time.deltaTime);
+    }
+
+
+void Update()
+    {
+      
+        Rotacion();
+        // Rotation on y axis
+    }
+}
