f41c6339cec350666744bcdecaaf791e3289403e
Sergio Ambriz Padilla <sergio.ampa@hotmail.com>
2021-04-30 15:41:32 +0000

    Añadadio un scriptvacio y un pop up de prueba

---
+++ Assets/Scripts/TutorialPopUps.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialPopUps : MonoBehaviour
+{
+    private new GameObject gameObject;
+    //popUp = gameObject.GetComponent<>
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            //PopUp.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            //PopUp.SetActive(false);
+        }
+    }
+}
+
