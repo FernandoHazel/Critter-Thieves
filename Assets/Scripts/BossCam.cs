1bf87bf07939341a8b153ce258dbc866f60d5e2e
Emerson Rios Diaz <rios.emerson29@hotmail.com>
2021-05-19 02:53:41 +0000

    No Comments

--- Assets/Scripts/BossCam.cs
+++ Assets/Scripts/BossCam.cs
@@ -10,6 +10,8 @@ public class BossCam : MonoBehaviour
     public float smoothTime;  //this is the time of delay of the camera movement
     private Vector2 velocity;
 
+    public Transform End;
+
 
 
     // Start is called before the first frame update
@@ -21,8 +23,14 @@ public class BossCam : MonoBehaviour
 
     void Rutas()
     {
+        float posX = transform.position.x;
         float posY = Mathf.SmoothDamp(transform.position.y, tracker.transform.position.y + 2.5f, ref velocity.y, smoothTime);
         transform.position = new Vector3(transform.position.x + (Time.deltaTime*5), Mathf.Clamp(posY, minCamPos.y, maxCamPos.y), transform.position.z);
+
+        if (transform.position.x == End.position.x)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z); ;
+        }
 
     }
     // Update is called once per frame
@@ -30,10 +38,5 @@ public class BossCam : MonoBehaviour
     {
         Rutas();
 
-        //float posY = Mathf.SmoothDamp(transform.position.y, tracker.transform.position.y + 2.5f, ref velocity.y, smoothTime);
-
-        //We move the camera to that position
-        //We use the "Clampt" function to limit the camera movement between a minimum and a maximum position in X and Y axis
-        //transform.position = new Vector3( 1 * Time.deltaTime, Mathf.Clamp(posY, minCamPos.y, maxCamPos.y), transform.position.z);
     }
 }
