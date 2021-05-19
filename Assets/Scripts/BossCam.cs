e30e6a29d50cc8fb1a08d1369f60b85c55c36b52
Emerson Rios Diaz <rios.emerson29@hotmail.com>
2021-05-19 02:06:46 +0000

    No Comments

--- Assets/Scripts/BossCam.cs
+++ Assets/Scripts/BossCam.cs
@@ -4,6 +4,26 @@ using UnityEngine;
 
 public class BossCam : MonoBehaviour
 {
+
+    public GameObject tracker;
+    public Vector2 minCamPos, maxCamPos;
+    public float smoothTime;  //this is the time of delay of the camera movement
+    private Vector2 velocity;
+
+
+
+    public float speed = 0f;  //velocidad de cubo
+
+    public float margenError = 0f;
+
+    int estados = 0;
+
+    public int modos = 0;
+
+    bool irAdelante = true;
+
+    bool loop = true;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,23 +33,16 @@ public class BossCam : MonoBehaviour
 
     void Rutas()
     {
-        transform.position = Vector3.Lerp(transform.position, waypoints[estados].position, speed * Time.deltaTime);
+        transform.position = Vector3.Lerp(transform.position, , speed * Time.deltaTime);
 
-        if (Vector3.Distance(transform.position, waypoints[estados].position) < margenError)
-        {
-            estados++;
-
-            if (estados >= waypoints.Length)
-            {
-                estados--;
-                modos = -1;
-                //Debug.Log("Detente");
-            }
-        }
     }
     // Update is called once per frame
     void Update()
-    {
-        
+    {;
+        float posY = Mathf.SmoothDamp(transform.position.y, tracker.transform.position.y + 2.5f, ref velocity.y, smoothTime);
+
+        //We move the camera to that position
+        //We use the "Clampt" function to limit the camera movement between a minimum and a maximum position in X and Y axis
+        transform.position = new Vector3(Mathf.Clamp(posY, minCamPos.y, maxCamPos.y), transform.position.z);
     }
 }
