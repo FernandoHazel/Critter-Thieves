de6b48fb7bf1cea2c73e418da3d6c3a3f36d801f
Emerson Rios Diaz <rios.emerson29@hotmail.com>
2021-05-19 03:02:39 +0000

    No Comments

--- Assets/Scripts/BossCam.cs
+++ Assets/Scripts/BossCam.cs
@@ -23,13 +23,16 @@ public class BossCam : MonoBehaviour
 
     void Rutas()
     {
-        float posX = transform.position.x;
         float posY = Mathf.SmoothDamp(transform.position.y, tracker.transform.position.y + 2.5f, ref velocity.y, smoothTime);
-        transform.position = new Vector3(transform.position.x + (Time.deltaTime*6.3f), Mathf.Clamp(posY, minCamPos.y, maxCamPos.y), transform.position.z);
 
         if (transform.position.x == End.position.x)
         {
-            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z); ;
+            transform.position = new Vector3(transform.position.x, Mathf.Clamp(posY, minCamPos.y, maxCamPos.y), transform.position.z);
+        }
+        else
+        {
+           
+            transform.position = new Vector3(transform.position.x + (Time.deltaTime * 50), Mathf.Clamp(posY, minCamPos.y, maxCamPos.y), transform.position.z);
         }
 
     }
