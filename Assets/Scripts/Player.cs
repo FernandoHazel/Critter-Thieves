5ad5a619916a57bd8475a0a1582c734fc6738e0c
Emerson Rios Diaz <rios.emerson29@hotmail.com>
2021-05-21 02:15:52 +0000

    No Comments

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -167,7 +167,7 @@ public class Player : MonoBehaviour
     {
         //Debug.Log(transform.position.x - BossCam.position.x);
 
-        if (transform.position.x - BossCam.position.x <= -15)
+        if (transform.position.x - BossCam.position.x == -14)
         {
             Die();
             Debug.Log("You died");
