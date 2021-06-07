7ddd083d4362eba3c644b2e6ff4184b770331379
Emerson Rios Diaz <rios.emerson29@hotmail.com>
2021-06-07 23:08:19 +0000

    even more sounds!

--- Assets/Scripts/AnimationSounds.cs
+++ Assets/Scripts/AnimationSounds.cs
@@ -6,10 +6,12 @@ public class AnimationSounds : MonoBehaviour
 {
 
     private AudioSource footstep;
+
     // Start is called before the first frame update
     void Start()
     {
         footstep = GetComponent<AudioSource>();
+
     }
 
     public void Footstep()
