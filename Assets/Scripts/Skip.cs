032115a69417661944b7da89a82ef8657af35ba9
Emerson Rios Diaz <rios.emerson29@hotmail.com>
2021-06-10 22:57:28 +0000

    Intro Complete, Start Button, Volume and Restart Button

--- Assets/Scripts/Skip.cs
+++ Assets/Scripts/Skip.cs
@@ -1,17 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class Skip : MonoBehaviour
 {
-    [SerializeField] private float delayBeforeLoad = 36f;
+    [SerializeField] private float delayBeforeLoad = 34f;
+
+    [SerializeField] private float delayBeforeText = 7f;
+
+    public Text instruction;
 
     private float timeCounter;
     // Start is called before the first frame update
     void Start()
     {
-        
+        instruction.enabled = false;
     }
 
     public void SkipScene()
@@ -19,11 +24,20 @@ public class Skip : MonoBehaviour
         SceneManager.LoadScene("LevelBuilder");
     }
 
+    void Intructions()
+    {
+        if (timeCounter > delayBeforeText)
+        {
+            instruction.enabled = true;
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
-        timeCounter += Time.deltaTime;  
-
+        timeCounter += Time.deltaTime;
+        Intructions();
+          
         if(timeCounter > delayBeforeLoad)
         {
             SceneManager.LoadScene("LevelBuilder");
